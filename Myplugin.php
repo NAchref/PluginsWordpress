96e2a421d4d70b0abbefe62c59f0fd41482fea6f
NAchref <64566472+NAchref@users.noreply.github.com>
2022-03-21 19:18:09 +0100

    Create Myplugin.php

---
+++ Myplugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       PLUGIN NAME
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0
+ */
+// Remove the admin bar from the front end
+add_filter( 'show_admin_bar', '__return_false' );
+
+
+
+
+?>
