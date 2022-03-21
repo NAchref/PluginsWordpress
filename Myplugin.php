d16380b4cda5809bd05fc34056002990b16b2e16
NAchref <64566472+NAchref@users.noreply.github.com>
2022-03-21 19:19:34 +0100

    Update Myplugin.php

--- Myplugin.php
+++ Myplugin.php
@@ -1,9 +1,21 @@
 <?php
 /**
- * Plugin Name:       PLUGIN NAME
+ * Plugin Name:       My Basics Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.0
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
  */
+
+
 // Remove the admin bar from the front end
 add_filter( 'show_admin_bar', '__return_false' );
 
